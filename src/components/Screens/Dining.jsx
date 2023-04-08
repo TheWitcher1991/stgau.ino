01752ac450dd8cd679457f7286abda594f72802c
Фоменко Сергей Юрьевич <sergfoma007@mail.ru>
2023-04-08 20:32:52 +0300

    Попытка добавить фото в столовой

--- src/components/Screens/Dining.jsx
+++ src/components/Screens/Dining.jsx
@@ -1,12 +1,47 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, SafeAreaView, StyleSheet } from 'react-native'
+
+//const image_dining = {'assets/dining_room_zoom.png'}
+const styles = StyleSheet.create({
+    container: {
+      paddingTop: 50,
+    },
+    stretch: {
+      width: 50,
+      height: 200,
+      resizeMode: 'stretch',
+    },
+  });
+  
+  const DisplayAnImageWithStyle = () => {
+    return (
+      <View style={styles.container}>
+        <Image
+          style={styles.stretch}
+          source={require('./assets/green_tea.png')}
+        />
+      </View>
+    );
+  };
 
 const Dining = () => {
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        //<SafeAreaView style={dining_styles.container}>
+        <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
             <Text>Столовая</Text>
-        </View>
+        </SafeAreaView>
     )
 }
 
+//<ImageBackground source={image_dining} resizeMode="cover" style={styles.image}>
+//</ImageBackground>
+
+const dining_styles = Style.Sheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'center', 
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+})
 export default Dining
